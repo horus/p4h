455cef634bff72ad1c6336090fcf7ca432303a5f
horus <horus.li@gmail.com>
2023-06-23 23:40:46 +0800

    update

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,7 +8,7 @@
 module Lib (run) where
 
 import Control.Monad (guard, liftM2)
-import Foreign.C.String (newCString, peekCString, withCString)
+import Foreign.C.String (newCString, peekCString, withCAString)
 import Foreign.ForeignPtr (ForeignPtr, newForeignPtr)
 import Foreign.Marshal.Alloc (free)
 import Foreign.Marshal.Array (withArray)
@@ -25,64 +25,52 @@ data P4 = P4 (ForeignPtr ClientApi) (ForeignPtr ClientUser)
 C.context (C.cppCtx <> C.baseCtx <> C.fptrCtx <> C.cppTypePairs [("ClientApi", [t|ClientApi|]), ("HsClientUser", [t|ClientUser|])])
 C.include "<iostream>"
 C.include "p4/clientapi.h"
-C.include "p4/p4libs.h"
 C.include "p4/hsclientuser.h"
 
 run :: [String] -> IO (Either String String)
 run [] = return $ Left "p4h\n"
-run (subcmd : args') =
-  initClient >>= \case
-    Left err -> return $ Left err
-    Right p4 -> do
-      args <- mapM newCString args'
-      withCString subcmd $ \c ->
-        withArray args $ \argv -> do
-          let argc = fromIntegral (length args')
-          run' p4 c argc argv
-          mapM_ free args
-          getOutput2 p4 ret
+run (subcmd : args') = do
+  p4 <- newP4
+  setPort p4 "tcp:1666"
+  init' p4
+  args <- mapM newCString args'
+  withCAString subcmd $ \c ->
+    withArray args $ \argv -> do
+      let argc = fromIntegral (length args')
+      run' p4 c argc argv
+      mapM_ free args
+      getOutput2 p4 ret
   where
     ret a "" = Right a
     ret _ b = Left b
 
-initClient :: IO (Either String P4)
-initClient = do
-  (ptrErr, ptrUi, ptrClient) <- withPtrs_ $ \(err, ui, client) ->
+newP4 :: IO P4
+newP4 = do
+  (ptrUi, ptrClient) <- withPtrs_ $ \(ui, client) ->
     [C.block| void {
-            StrBuf msg;
-            Error e;
-            *$(char **err) = (char *)NULL;
-            *$(HsClientUser **ui) = new HsClientUser();
-            *$(ClientApi **client) = new ClientApi();
-            P4Libraries::Initialize( P4LIBRARIES_INIT_ALL, &e );
-            if( e.Test() ) {
-              e.Fmt( &msg );
-              *$(char **err) = strdup(msg.Text());
-              return;
+          *$(HsClientUser **ui) = new HsClientUser();
+          *$(ClientApi **client) = new ClientApi();
           }
-          (*$(ClientApi **client))->SetPort("tcp:1666");
-          (*$(ClientApi **client))->Init(&e);
-          if( e.Test() ) {
-            e.Fmt( &msg );
-            *$(char **err) = strdup(msg.Text());
-            return;
-          }
-        }
     |]
-  if ptrErr == nullPtr
-    then do
-      fpClient <- newForeignPtr [C.funPtr| void free(ClientApi *p) { delete p; } |] ptrClient
-      fpUi <- newForeignPtr [C.funPtr| void free(HsClientUser *p) { delete p; } |] ptrUi
-      return $ Right $ P4 fpClient fpUi
-    else do
-      err' <- peekCString ptrErr
-      [C.block| void {
-          delete $(HsClientUser *ptrUi);
-          delete $(ClientApi *ptrClient);
-          free($(char *ptrErr));
-        }
-      |]
-      return $ Left err'
+  fpClient <- newForeignPtr [C.funPtr| void free(ClientApi *p) { delete p; } |] ptrClient
+  fpUi <- newForeignPtr [C.funPtr| void free(HsClientUser *p) { delete p; } |] ptrUi
+  return $ P4 fpClient fpUi
+
+setPort :: P4 -> String -> IO ()
+setPort (P4 fpClient _) port' =
+  withCAString port' $ \port ->
+    [C.block| void { $fptr-ptr:(ClientApi *fpClient)->SetPort($(char *port)); } |]
+
+init':: P4 -> IO ()
+init' (P4 fpClient fpUi) =
+  [C.block| void {
+      Error e;
+      $fptr-ptr:(ClientApi *fpClient)->Init(&e);
+      if(e.Test())
+        $fptr-ptr:(HsClientUser *fpUi)->HandleError(&e);
+    }
+  |]
+
 
 run' ::
   P4 ->
@@ -94,17 +82,13 @@ run' (P4 fpClient fpUi) cmd argc argv =
   [C.block| void {
       HsClientUser *ui = $fptr-ptr:(HsClientUser *fpUi);
       ClientApi *client = $fptr-ptr:(ClientApi *fpClient);
-      StrBuf msg;
-      Error e;
       if ($(int argc) > 0)
         client->SetArgv($(int argc), $(char **argv));
       client->Run( $(const char *cmd), ui );
-      if ( client->Final( &e ) > 0 ) {
-        if (e.Test()) {
-          e.Fmt( &msg );
-          ui->OutputError( msg.Text() );
-        }
-      }
+      Error e;
+      client->Final(&e);
+      if (e.Test())
+        ui->HandleError(&e);
     }
   |]
 
